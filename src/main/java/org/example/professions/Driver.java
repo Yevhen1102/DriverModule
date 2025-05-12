99525535d4720b4f116c7e7dd92798f08ce239fd
Yevhen Arutiunian <arytynyanevgen@gmail.com>
2025-05-13 00:10:40 +0300

    DriverModule adding for com.company project

---
+++ src/main/java/org/example/professions/Driver.java
@@ -0,0 +1,20 @@
+package org.example.professions;
+
+public class Driver {
+    String message1 = "I\'m driving!";
+    private String carModel;
+
+    public Driver(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public void displayInfo1() {
+        System.out.println(message1);
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("CarModel = " + this.carModel);
+    }
+
+
+}
